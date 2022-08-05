8a3cb020eb87114bbff25cb515a57f8b11c45b44
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-08-05 15:16:35 +0900

    path.js

--- path.js
+++ path.js
@@ -6,25 +6,36 @@ const dirname=__dirname;
 console.log(filename);
 console.log(dirname);
 
-console.log(path.sep);
+console.log('path.sep: ',path.sep);
 //경로의 구분자
-
-console.log(path.dirname(filename));
+console.log('path.dirname: ',path.dirname(filename));
 //파일이 위치한 폴더 경로를 보여줌
-
-console.log(path.extname(filename));
+console.log('path.extname: ',path.extname(filename));
 //파일의 확장자를 보여줌
-
-console.log(path.basename(filename, path.extname(filename)));
+console.log('path.basename: ',path.basename(filename, path.extname(filename)));
 //파일의 이름 보여줌
-
-console.log(path.parse(filename));
+console.log('path.parse: ',path.parse(filename));
 //파일 경로는 root, dir, base, ext, name으로 분리
-
-console.log(path.format({
+console.log('path.format: ',path.format({
     dir: '/Users/kimkyuri/Documents/wedatalap/node.js_study',
     name:'path',
     ext: '.js'
 }));
 //path.parse()한 객체를 파일 경로로 합침
 
+
+//자주 사용할 함수
+
+console.log(path.normalize('Users//kimkyuri//////Documents///wedatalap/node.js_study'));
+// / 를 실수로 여러 번 사용했거나 혼용했을 때 정상적인 경로로 변환해준다.
+
+
+console.log(path.join('/foo/bar','/tmp/file/'));
+//인자로 받은 경로들을 하나의 문자열 형태로 리턴한다.
+console.log(path.resolve('/foo/bar','/tmp/file/'));
+//resolve도 join과 마찬가지로 인자로 받은 경로들을 문자열 형태로 리턴한다.
+
+
+console.log(path.join('/foo/bar','./baz'));
+console.log(path.resolve('/foo/bar','./baz'));
+//동일한 결과 (/로 시작하지 않기 때문에)
