3a33850e22f2e9a8df03474d4d13351761273cd3
kimkyuuuuri <rnflrnfl0633@naver.com>
2022-08-08 17:39:56 +0900

    url.js

---
+++ url.js
@@ -0,0 +1,15 @@
+const url = require('url');
+
+// node방식
+const parseUrl = url.parse('https://search.naver.com/search.naver?query=고려대학교');
+console.log(parseUrl);
+console.log(url.format(parseUrl));
+
+// WHATWG방식
+const {URL} = url;
+const myURL = new URL('https://search.naver.com/search.naver?query=고려대학교');
+console.log(myURL);
+console.log(url.format(myURL));  // node 방식과 다르게 username, password,origin,searchParam
+
+console.log('searchParams:',myURL.searchParams);
+
